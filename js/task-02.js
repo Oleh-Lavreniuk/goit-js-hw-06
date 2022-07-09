04dcf9b1ce7aaf409e217589151b6cb65bf117b2
Oleh-Lavreniuk <oleh4380211@gmail.com>
2022-07-09 23:32:43 +0300

    add hw-2
    completed hw-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,28 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulRef = document.querySelector("#ingredients");
+
+// !--- Variant-1 ---
+ingredients.forEach(function (el) {
+  const ingredient = document.createElement("li");
+  ingredient.classList.add("item");
+  ingredient.textContent = `${el}`;
+  ulRef.appendChild(ingredient);
+});
+
+// !--- Variant-2 ---
+// for (const elem of ingredients) {
+//   const ingredient = document.createElement("li");
+//   ingredient.classList.add("item");
+//   ingredient.textContent = `${elem}`;
+//   ulRef.appendChild(ingredient);
+// }
+
+console.log(ulRef);
