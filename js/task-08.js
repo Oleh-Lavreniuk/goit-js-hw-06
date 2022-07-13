f5085e055daee1e0795dd28ff51dfbf16f3020e8
Oleh-Lavreniuk <oleh4380211@gmail.com>
2022-07-13 19:18:53 +0300

    add task-8
    completed task-8 of 10

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(ev) {
+  ev.preventDefault();
+
+  const email = ev.currentTarget.elements.email.value.trim();
+  const password = ev.currentTarget.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("Все поля должны быть заполнены");
+    return;
+  }
+
+  const formData = { email, password };
+  console.log(formData);
+  form.reset();
+}
