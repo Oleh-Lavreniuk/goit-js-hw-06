51e31136928f391dd830b76ccba146b2e31e73fd
Oleh-Lavreniuk <oleh4380211@gmail.com>
2022-07-14 00:15:53 +0300

    add task-9
    completed task-9 of 10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const changeColorBtn = document.querySelector(".change-color");
+let spanColor = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  const takeColor = getRandomHexColor();
+  body.style.backgroundColor = takeColor;
+  spanColor.textContent = takeColor;
+}
